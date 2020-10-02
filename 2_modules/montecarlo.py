7c2074f4599e15c8c10445e5fe5a54dfb055226e
antounes <mba@tradelab.fr>
2020-10-02 15:40:03 +0200

    2.2 Modules - Quelques modules batteries included

--- 2_modules/montecarlo.py
+++ 2_modules/montecarlo.py
@@ -14,13 +14,13 @@ def f(x):
     return math.sqrt(1-x**2)
 
 print('-'*63)
-print('{0:10>s} | {1:^12s} | {2:^14s} | {3:^15s} |'.format('n',
-      'approximation', 'erreur absolue', 'erreur relative'))
+print('{0:>10s} | {1:^12s} | {2:^14s} | {3:^15s} |'
+      .format('n', 'approximation', 'erreur absolue', 'erreur relative'))
 print('-'*63)
 for i in range(0, 10, 2):
     n = 10**i
     approx = 4*montecarlo(f, 0, 1, n)
     erreur = math.pi - approx
     print('{0:10d} | {1: 12.10f} | {2: 14.10f} | {3: 15.10f} |'
-          .format(n, approx, erreur, abs(erreur/math.pi))
+          .format(n, approx, erreur, abs(erreur/math.pi)))
 print('-'*63)
